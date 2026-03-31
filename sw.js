c509ac151215e13daed1ffb2c3e9f175c3eb7da4
fryanbarrell666 <fryanbarrell@gmail.com>
2026-03-30 23:06:19 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE = 'bloom-v1';
+const CACHE = 'bloom-v2';
 const SHELL = [
-  '/',
-  '/index.html',
+  '/bloom/',
+  '/bloom/index.html',
   'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;1,300;1,400&family=DM+Sans:wght@300;400;500&display=swap'
 ];
 
